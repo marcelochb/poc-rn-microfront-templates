cd02be99488a59415d068f4c2dc6fe3d5018406f
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-02-19 12:11:44 -0300

    export pix and loan templates

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,2 +1,4 @@
 export * from './main/types';
 export * from './payment/types';
+export * from './loan/types';
+export * from './pix/types';
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './main';
 export * from './payment';
+export * from './loan';
+export * from './pix';
