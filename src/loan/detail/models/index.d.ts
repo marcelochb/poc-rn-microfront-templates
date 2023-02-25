90d4a0cd35dc1a5d59c401a76c69408143063642
Marcelo Block Teixeira <marcelochb@gmail.com>
2023-02-24 23:09:09 -0300

    feat: loan detail

--- src/loan/detail/models/index.d.ts
+++ src/loan/detail/models/index.d.ts
@@ -1,8 +1,12 @@
+import { LoanEntity } from "@poc/core";
 import { ModelOfTheme } from "@poc/theme"
 import { Component } from "react"
 
 export type ModelOfLoanDetailTemplate = {
-  theme: ModelOfTheme
+  theme: ModelOfTheme;
+  loading: boolean;
+  error: boolean;
+  loan: LoanEntity;
 }
 
 export class LoanDetailTemplate extends Component<ModelOfLoanDetailTemplate> {};
--- src/loan/detail/view/index.tsx
+++ src/loan/detail/view/index.tsx
@@ -1,15 +1,52 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 import { ModelOfLoanDetailTemplate } from '../models'
 import { PageWrapper, Texts } from '@poc/ui'
+import { ModelOfTheme } from '@poc/theme'
 
-export const LoanDetailTemplate: React.FC<ModelOfLoanDetailTemplate> = ({theme}) => {
+export const LoanDetailTemplate: React.FC<ModelOfLoanDetailTemplate> = ({
+  theme,
+  loading,
+  error,
+  loan
+}) => {
   return (
-    <PageWrapper theme={theme} isStatusBarLight>
-      <Texts.Title theme={theme}>LoanDetailTemplate</Texts.Title>
+    <PageWrapper
+      theme={theme}
+      isStatusBarLight
+      error={error}
+      loading={loading}
+    >
+      <View style={styles(theme).container}>
+        <Texts.SubTitle style={styles(theme).label} theme={theme}>Nome</Texts.SubTitle>
+        <Texts.Body style={styles(theme).value} theme={theme}>{loan.name}</Texts.Body>
+        <Texts.SubTitle style={styles(theme).label} theme={theme}>Tipo</Texts.SubTitle>
+        <Texts.Body style={styles(theme).value} theme={theme}>{loan.type}</Texts.Body>
+        <Texts.SubTitle style={styles(theme).label} theme={theme}>Valor</Texts.SubTitle>
+        <Texts.Body style={styles(theme).value} theme={theme}>{loan.amount}</Texts.Body>
+      </View>
     </PageWrapper>
   )
 }
 
 
-const styles = StyleSheet.create({})
+const styles = (theme:ModelOfTheme) => {
+  const {metrics, colors, fontFamily,fontSize} = theme;
+ return StyleSheet.create({
+  container: {
+    padding: metrics.base,
+    margin: metrics.base,
+    borderColor: colors.elementsSecundary,
+    borderWidth: 0.2,
+    borderRadius: metrics.radiusBase
+  },
+  label: {
+    color: colors.elementsSecundary,
+    fontFamily: fontFamily.italic
+  },
+  value: {
+    fontSize: fontSize.large,
+    marginBottom: metrics.large
+  }
+})
+}
